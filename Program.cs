2286f35efa2301ef0ecbbfcfcc90dd92d408b0eb
Nathan <nathand3v@gmail.com>
2026-07-14 09:08:04 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Wedding.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+
+app.Run();
